46ff25a7835f6dfef61bd753406230c720c580c2
SamaaraG <samaraagallegos@gmail.com>
2025-12-12 14:51:04 -0600

    Loba

---
+++ Constantes.py
@@ -0,0 +1,15 @@
+ANCHO=900
+ALTO=750
+#Ventana
+ALTO_PERSONAJE = 20
+ANCHO_PERSONAJE = 20
+COLOR_BG = (0,0,20)
+Blue = (0,0,255)
+FPS = 80
+VELOCIADAD = 20
+SCALA_PERSONAJE=0.5
+scroll_x = 0  # Posición X actual del fondo, usada para el desplazamiento
+scroll_speed = 5 
+#Dimensiones de los obstaculos
+ANCHO_OBS = 50
+ALTO_OBS = 50
